9232d4d95d45b47f9223fe7983bf5603d801b4eb
freegfw <weiwei25988@gmail.com>
2020-07-21 17:52:16 +0800

    Site updated: 2020-07-21 17:52:16

--- sw.js
+++ sw.js
@@ -26,15 +26,15 @@ var precacheUrls = [];
 
   precacheUrls.push('/posts/ed4a4be9.html');
 
+  precacheUrls.push('/posts/186d4656.html');
+
   precacheUrls.push('/posts/aba45e6e.html');
-
-  precacheUrls.push('/posts/aba66035.html');
-
-  precacheUrls.push('/posts/ce7f07f4.html');
 
   precacheUrls.push('/posts/614351eb.html');
 
-  precacheUrls.push('/posts/186d4656.html');
+  precacheUrls.push('/posts/ce7f07f4.html');
+
+  precacheUrls.push('/posts/aba66035.html');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":30};
