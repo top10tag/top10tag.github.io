f40c5c2acf6c098427bab101d50a3147bb7df1ae
freegfw <weiwei25988@gmail.com>
2020-07-22 15:03:17 +0800

    Site updated: 2020-07-22 15:03:17

--- sw.js
+++ sw.js
@@ -26,13 +26,13 @@ var precacheUrls = [];
 
   precacheUrls.push('/posts/ed4a4be9.html');
 
-  precacheUrls.push('/posts/ce7f07f4.html');
-
-  precacheUrls.push('/posts/186d4656.html');
+  precacheUrls.push('/posts/aba45e6e.html');
 
   precacheUrls.push('/posts/614351eb.html');
 
-  precacheUrls.push('/posts/aba45e6e.html');
+  precacheUrls.push('/posts/186d4656.html');
+
+  precacheUrls.push('/posts/ce7f07f4.html');
 
   precacheUrls.push('/posts/aba66035.html');
 
